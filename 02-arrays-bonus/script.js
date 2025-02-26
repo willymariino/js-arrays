84abfd14ab09be4c15a7fae80d9383735fc763b5
willymariino <wg.mariino@gmail.com>
2025-02-26 18:23:11 +0100

    bonus, terzo esercizio

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,9 +17,11 @@ const reversedTeachers = teachers;
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-const longNames = null;
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const splicedTeacher = teachers.splice(1); //non ho capito perchè negli esempi in rete usavano due 1 tipo: splice(1, 1)
+console.log(splicedTeacher);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
