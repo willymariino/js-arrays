5423564bb7b27d5d9f95e044538d3a26e5a14094
willymariino <wg.mariino@gmail.com>
2025-02-26 17:24:16 +0100

    quarto

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,17 +19,27 @@ console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const removedTeacher = teachers.pop();
+console.log(teachers);
+const lastTeacher = removedTeacher;
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers);
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
+const LewisIndex = teachers.indexOf('Lewis');
 const lewisIndex = null;
 
 // 8. Verifica se l'array teachers è vuoto
